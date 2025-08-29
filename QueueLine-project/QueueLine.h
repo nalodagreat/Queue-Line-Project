b06c922496653bbc54cecfedce8aeeaef95f7275
nalodagreat <nalothegreat0@gmail.com>
2025-08-29 23:23:09 +0200

    isuue a Ticket implementation -data membere -constractor with serveTime and prefix -prefix generate automaticly

--- QueueLine-project/QueueLine.h
+++ QueueLine-project/QueueLine.h
@@ -1,5 +1,33 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 class QueueLine
 {
+	string prefix;
+	int TotalTickets=0;
+	int sevedClients=0;
+	int waitClients=0;
+	int serveTime;
+	short prefixContactor=0;
+	string _generatePrefix(string prefix)
+	{
+		prefix += to_string(prefixContactor);
+		prefixContactor++;
+		return prefix;
+	}
+public:
+	QueueLine(string prefix, int serveTime)
+	{
+		this->prefix = prefix;
+		this->serveTime = serveTime;
+	}
+	void issueTicket()
+	{
+		_generatePrefix(prefix);
+		TotalTickets++;
+		sevedClients++;
+		waitClients++;
+	}
 };
 
