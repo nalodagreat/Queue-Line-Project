2358eb6248f2733dbae036469f4dbb67c77cfe2e
nalodagreat <nalothegreat0@gmail.com>
2025-08-31 16:18:26 +0200

    QUEUELINE with inner class using inner class ticket instead of struct making code cleaner

--- QueueLine-project/QueueLine.h
+++ QueueLine-project/QueueLine.h
@@ -7,117 +7,134 @@
 
 using namespace std;
 
-class QueueLine {
-    struct Ticket {
-        string prefix;
-        string date;
-        string time;
-        int waitClients;
-        int serveTime;
-    };
+class QueueLine 
+{
+	class Ticket
+	{
+		int PrefixContactor = 0;
+		string ticketNumber;
+		string dateOfTicket;
+		string timeOfTicket;
+		int waitaingClients;
+		int whenMyTurn;
+	public:
+		Ticket(string ticketNumber,int PrefixContactor,int waitaingClients, int whenMyTurn)
+		{
+			this->ticketNumber = ticketNumber;
+			this->dateOfTicket = Date::convertDateOfLoginstructToString(Date::getSystemDate());
+			this->waitaingClients = waitaingClients;
+			this->timeOfTicket = Date::getSystemTime();
+			this->whenMyTurn = whenMyTurn;
+			this->PrefixContactor = PrefixContactor;
+		}
+		string getTicketNumber()
+		{
+			return ticketNumber += to_string(PrefixContactor);
+		}
+		string getDateOfTicket()
+		{
+			return this->dateOfTicket;
+		}
+		string getTimeOfTicket()
+		{
+			return this->timeOfTicket;
+		}
+		int getWhenMyTurn()
+		{
+			return  whenMyTurn * waitaingClients;
+		}
+		void printTicket()
+		{
+			cout << "\t\t\t-----------------------------------\n";
+			
+				cout << "\t\t\t" << getTicketNumber() << "\n";
+				cout << "\t\t\tDate: " << dateOfTicket << "\n";
+				cout << "\t\t\tTime: " << timeOfTicket << "\n";
+				cout << "\t\t\tWaiting clients: " << waitaingClients << "\n";
+				cout << "\t\t\tServe in time: " << getWhenMyTurn() << "\n";
 
-    queue<Ticket> tickets;
-    string prefix;
-    int TotalTickets = 0;
-    int servedClients = 0;
-    short prefixCounter = 1;
-    int standardServeTime;
+				cout << "\t\t\t-----------------------------------\n";
+		}
 
-    string _generatePrefix(string prefix) {
-        prefix += to_string(prefixCounter++);
-        return prefix;
-    }
 
+
+
+
+	};
+	queue <Ticket> tickets;
+	string prefix = "";
+	int totalTickets=0;
+	int servedClients=0;
+	int StandardTimeToServe;
+	int waitaingClients()
+	{
+		return tickets.size();
+	}
 public:
-    QueueLine(string prefix, int serveTime) {
-        this->prefix = prefix;
-        this->standardServeTime = serveTime;
-    }
+	QueueLine(string prefix,int StandardTimeToServe)
+	{
+		this->prefix = prefix;
+		this->StandardTimeToServe = StandardTimeToServe;
+	}
+	void issueTicket()
+	{
+		totalTickets++;
+		tickets.push(Ticket(prefix, totalTickets, waitaingClients(),StandardTimeToServe));
+	}
+	void printInfo() 
+	{
+		cout << "Pay Bills Info:\n";
+		cout << "\t\t\t_________________________________________________________\n";
+		cout << "\n\t\t\t\t\tQUEUE INFO\n";
+		cout << "\t\t\t_________________________________________________________\n";
+		cout << "\t\t\tprefix : " << prefix << endl;
+		cout << "\t\t\tTotalTickets : " << totalTickets<< endl;
+		cout << "\t\t\tservedClients : " << servedClients << endl;
+		cout << "\t\t\t waitaingClients : " << waitaingClients()<< endl;
+		cout << "\t\t\t----------------------------\n";
+	}
+	void printTicketsRTL() {
+		queue<Ticket> temp = tickets;
+		cout << "\n\t\t\tTickets: ";
+		while (!temp.empty()) 
+		{
+			cout << temp.front().getTicketNumber() << " <-- ";
+			temp.pop();
+		}
+		cout << endl;
+	}
 
-    void issueTicket() {
-        Ticket tckt;
-        tckt.prefix = _generatePrefix(prefix);
-        tckt.date = Date::convertDateOfLoginstructToString(Date::getSystemDate());
-        tckt.time = Date::getSystemTime();
+	void printTicketsLTR() {
+		queue<Ticket> temp = tickets;
+		stack<Ticket> s;
+		while (!temp.empty()) {
+			s.push(temp.front());
+			temp.pop();
+		}
 
-        if (TotalTickets == 0) {
-            tckt.serveTime = 0;
-            tckt.waitClients = 0;
-        }
-        else {
-            tckt.serveTime = tickets.back().serveTime + standardServeTime;
-            tckt.waitClients = tickets.back().waitClients + 1;
-        }
-
-        tickets.push(tckt);
-        TotalTickets++;
-    }
-
-    void printInfo() {
-        cout << "Pay Bills Info:\n";
-        cout << "\t\t\t_________________________________________________________\n";
-        cout << "\n\t\t\t\t\tQUEUE INFO\n";
-        cout << "\t\t\t_________________________________________________________\n";
-        cout << "\t\t\tprefix : " << prefix << endl;
-        cout << "\t\t\tTotalTickets : " << TotalTickets << endl;
-        cout << "\t\t\tservedClients : " << servedClients << endl;
-        cout << "\t\t\t----------------------------\n";
-    }
-
-    void printTicketsRTL() {
-        queue<Ticket> temp = tickets;
-        cout << "\n\t\t\tTickets: ";
-        while (!temp.empty()) {
-            cout << temp.front().prefix << " <-- ";
-            temp.pop();
-        }
-        cout << endl;
-    }
-
-    void printTicketsLTR() {
-        queue<Ticket> temp = tickets;
-        stack<Ticket> s;
-        while (!temp.empty()) {
-            s.push(temp.front());
-            temp.pop();
-        }
-
-        cout << "\n\t\t\tTickets: ";
-        while (!s.empty()) {
-            cout << s.top().prefix << " --> ";
-            s.pop();
-        }
-        cout << endl;
-    }
-
-    void printAllTickets() {
-        queue<Ticket> temp = tickets;
-        cout << "\n\t\t\t\t---Tickets---:\n";
-        cout << "\t\t\t-----------------------------------\n";
-
-        while (!temp.empty()) {
-            Ticket tckt = temp.front();
-            cout << "\t\t\t" << tckt.prefix << "\n";
-            cout << "\t\t\tDate: " << tckt.date << "\n";
-            cout << "\t\t\tTime: " << tckt.time << "\n";
-            cout << "\t\t\tWaiting clients: " << tckt.waitClients << "\n";
-            cout << "\t\t\tServe in time: " << tckt.serveTime << "\n";
-            cout << "\t\t\t-----------------------------------\n";
-            temp.pop();
-        }
-    }
-    void serveNextClient()
-    {
-        Ticket served = tickets.front();
-        tickets.pop();
-        servedClients++;
-        queue<Ticket> temp;
-        while (!tickets.empty()) {
-            Ticket tckt = tickets.front();
-            tickets.pop();
-            tckt.waitClients = max(0, tckt.waitClients - 1); 
-            temp.push(tckt);
-        }
-        tickets = temp;
-    }
+		cout << "\n\t\t\tTickets: ";
+		while (!s.empty()) {
+			cout << s.top().getTicketNumber() << " --> ";
+			s.pop();
+		}
+		cout << endl;
+	}
+	void printAllTickets()
+	{
+		queue<Ticket> temp = tickets;
+		cout << "\n\n\t\t\t\t---Tickets---:\n";
+		while (!temp.empty())
+		{
+			temp.front().printTicket();
+			temp.pop();
+		}
+	}
+	void serveNextClient()
+	{
+		if (!tickets.empty())
+		{
+			tickets.pop();
+			servedClients++;
+		}
+	}
 };
